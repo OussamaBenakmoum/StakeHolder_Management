1172e9177da3a45468378d38e2b24b0a5f5d0311
OussamaBenakmoum <io_benakmoum@esi.dz>
2022-03-28 20:40:23 +0100

    First module configurations

---
+++ __manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# //////////////////////////////////////////////////////////////////////////////
+#
+#    ESI ALger.
+#
+#    Copyright (C) 2020-TODAY Emploitic(<https://www.emploitic.com>).
+#    Author: Oussama BENAKMOUME && TAHAR Noureddine,
+#
+# //////////////////////////////////////////////////////////////////////////////
+
+{
+    'name': "Stackeholder Management",
+    'version': '1.0.0',
+    'summary': """Gestion des parties prenantes""",
+    'description': """
+       Module de gestion du registre des parties prenantes, TP pour un module de gestion de changement dans un projet.
+    """,
+    'author': 'BENAKMOUME Oussama AND TAHAR Noureddine',
+    'company': 'ESI Alger',
+    'depends': [],
+    'data': [
+        'views/',
+        'security/ir.model.access.csv',
+
+    ],
+    'demo': [],
+    'images': [],
+    'installable': True,
+    'application': True
+}
